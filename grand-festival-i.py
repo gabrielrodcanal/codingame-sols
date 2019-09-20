02f1d717b8b15c9da74f02a68436477cdb6de09d
Gabriel Rodríguez Canal <gabriel.rodriguez.canal@gmail.com>
2019-09-21 01:45:02 +0200

    Grand Festival I

---
+++ grand-festival-i.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+# Total prize starting on day d with rest r.
+def prize(d,r):
+    if(d == N):
+        return 0
+    if(r == 0):
+        return prize(d+1,R)
+    
+    if memo[d+1][R] == -1:
+        prize_excluded = prize(d+1,R)
+        memo[d+1][R] = prize_excluded
+    else:
+        prize_excluded = memo[d+1][R]
+    if memo[d+1][r-1] == -1:
+        prize_included = p[d] + prize(d+1,r-1)
+        memo[d+1][r-1] = prize_included
+    else:
+        prize_included = memo[d+1][r-1]
+
+    if(prize_excluded < prize_included):
+        print(d, file=sys.stderr)
+
+    return max(prize_excluded, prize_included)
+
+if __name__ == "__main__":
+    N = int(input())
+    R = int(input())
+
+    p = []
+
+    for i in range(N):
+        p.append(int(input()))
+
+    memo = [[-1] * (R+1) for _ in range(N+1)]
+
+    print(prize(0,R))
